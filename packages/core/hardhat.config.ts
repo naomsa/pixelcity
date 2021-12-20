56d406f43ad0b5384f79997edd367edda0bc9a05
naomsa <omar@na2.com.br>
2021-12-20 12:04:40 -0300

    chore: edited configs

--- packages/core/hardhat.config.ts
+++ packages/core/hardhat.config.ts
@@ -11,28 +11,28 @@ import "solidity-coverage";
 dotenv.config();
 
 const config: HardhatUserConfig = {
-	solidity: {
-		version: "0.8.9",
-		settings: {
-			optimizer: { enabled: true, runs: 200 },
-		},
-	},
-	networks: {
-		rinkeby: {
-			url: process.env.RPC_URL || "",
-			accounts:
-				process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-		},
-	},
-	gasReporter: {
-		enabled: process.env.REPORT_GAS !== undefined,
-		currency: "USD",
-		coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-		excludeContracts: ["mocks/"],
-	},
-	etherscan: {
-		apiKey: process.env.ETHERSCAN_API_KEY,
-	},
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+    },
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.RPC_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    excludeContracts: ["mocks/"],
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
