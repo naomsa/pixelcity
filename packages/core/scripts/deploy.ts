349c547d408574a74a4ef6b87ee20617fc0e97ed
naomsa <omar@na2.com.br>
2021-12-20 12:08:51 -0300

    feat: added sale activation

--- packages/core/scripts/deploy.ts
+++ packages/core/scripts/deploy.ts
@@ -4,35 +4,35 @@ import fillTraits from "../helpers/fillTraits";
 
 const VERIFY = network.name !== "localhost";
 async function main() {
-	const descriptor = await (
-		await ethers.getContractFactory("PixelCityDescriptor")
-	).deploy("__ipfs__", "__lore__");
+  const descriptor = await (
+    await ethers.getContractFactory("PixelCityDescriptor")
+  ).deploy("__ipfs__", "__lore__");
+  await descriptor.deployed();
 
-	await descriptor.deployed();
+  const token = await (
+    await ethers.getContractFactory("PixelCityToken")
+  ).deploy(descriptor.address);
+  await token.deployed();
 
-	const token = await (
-		await ethers.getContractFactory("PixelCityToken")
-	).deploy(descriptor.address);
-	await token.deployed();
+  console.log("Token deployed to:", token.address);
+  console.log("Descriptor deployed to:", descriptor.address);
 
-	console.log("Token deployed to:", token.address);
-	console.log("Descriptor deployed to:", descriptor.address);
+  await fillTraits(descriptor);
+  await token.setIsActive(true);
 
-	await fillTraits(descriptor);
-
-	if (VERIFY) {
-		await run("verify:verify", {
-			address: descriptor.address,
-			constructorArguments: ["__ipfs__", "__lore__"],
-		});
-		await run("verify:verify", {
-			address: token.address,
-			constructorArguments: [descriptor.address],
-		});
-	}
+  if (VERIFY) {
+    await run("verify:verify", {
+      address: descriptor.address,
+      constructorArguments: ["__ipfs__", "__lore__"],
+    });
+    await run("verify:verify", {
+      address: token.address,
+      constructorArguments: [descriptor.address],
+    });
+  }
 }
 
 main().catch((error) => {
-	console.error(error);
-	process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
